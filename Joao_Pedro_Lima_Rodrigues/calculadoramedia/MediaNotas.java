c0ff93c6ed23f1c9915ac26752610866e8c1acb0
João Pedro Lima Rodrigues <jplr16@gmail.com>
2023-08-16 19:22:10 -0300

    correcoes trabalho

--- Joao_Pedro_Lima_Rodrigues/calculadoramedia/MediaNotas.java
+++ Joao_Pedro_Lima_Rodrigues/calculadoramedia/MediaNotas.java
@@ -1,8 +1,8 @@
 public class MediaNotas {
     public static void main(String[] args) {
         double[][] notas = {
-            {8.0, 9.0, 8.6, 9.2, 7.0},
-            {7.1, 6.0, 7.2, 5.2, 7.4}
+                { 8.0, 9.0, 8.6, 9.2, 7.0 },
+                { 7.1, 6.0, 7.2, 5.2, 7.4 }
         };
 
         double[] medias = new double[2];
