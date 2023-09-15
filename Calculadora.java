e13366970fcd19d6537b7cda3ce28c0113a84a18
Gustavo Sganderla <110643171+Gsgandela@users.noreply.github.com>
2023-09-14 21:15:31 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+    public double adicionar(double numero1, double numero2) {
+        return numero1 + numero2;
+    }
+
+    public double subtrair(double numero1, double numero2) {
+        return numero1 - numero2;
+    }
+
+    public double multiplicar(double numero1, double numero2) {
+        return numero1 * numero2;
+    }
+
+    public double dividir(double numero1, double numero2) {
+        if (numero2 != 0) {
+            return numero1 / numero2;
+        } else {
+            throw new ArithmeticException("Não é possível dividir por zero.");
+        }
+    }
+}
+
