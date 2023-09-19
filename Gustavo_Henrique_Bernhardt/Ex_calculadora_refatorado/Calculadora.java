e41b20dd993d8df949ef060912babca622175af4
ghbernhardt <116513654+ghbernhardt@users.noreply.github.com>
2023-09-19 19:41:20 -0300

    Create Calculadora.java

---
+++ Gustavo_Henrique_Bernhardt/Ex_calculadora_refatorado/Calculadora.java
@@ -0,0 +1,30 @@
+public class Calculadora {
+    private double num1;
+    private double num2;
+
+    public Calculadora(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double somar() {
+        return num1 + num2;
+    }
+
+    public double subtrair() {
+        return num1 - num2;
+    }
+
+    public double multiplicar() {
+        return num1 * num2;
+    }
+
+    public double dividir() {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            System.out.println("Valor Inválido");
+            return Double.NaN;
+        }
+    }
+}
