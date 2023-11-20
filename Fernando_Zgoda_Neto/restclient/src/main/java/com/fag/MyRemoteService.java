a6f0c5f4adf1798754207ff6677791f4307298bc
fernando zgoda neto <fzneto@minha.fag.edu.br>
2023-11-20 13:37:39 -0300

    Trabalho DTO's

---
+++ Fernando_Zgoda_Neto/restclient/src/main/java/com/fag/MyRemoteService.java
@@ -0,0 +1,33 @@
+package com.fag;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import com.fag.dto.ConsultResponseDTO;
+import com.fag.dto.ConsultaBoletoDTO;
+import com.fag.dto.PaymentResponseDTO;
+import com.fag.dto.TokenDTO;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Form;
+import jakarta.ws.rs.core.MediaType;
+
+@RegisterRestClient(baseUri = "https://sandbox.openfinance.celcoin.dev")
+public interface MyRemoteService {
+
+    @POST
+    @Path("/v5/token")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    TokenDTO getToken(Form form);
+
+    @POST
+    @Path("/v5/transactions/billpayments/authorize")
+    ConsultResponseDTO consult(@HeaderParam("Authorization") String token, ConsultaBoletoDTO dto);
+
+    @POST
+    @Path("/v5/transactions/billpayments")
+    PaymentResponseDTO payment(@HeaderParam("Authorization") String token, ConsultaBoletoDTO dto);
+ 
+}
